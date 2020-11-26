c537b5a2ca76f9922324680072ff114751188c18
Luis Pedro Coelho <luis@luispedro.org>
2020-11-26 16:10:42 +0800

    RFCT Split off FASTA parsing to fasta.py
    - Use gzip throughout
    - Other small refactors

--- GMSC_10/2_cal_dedup_sub.py
+++ GMSC_10/2_cal_dedup_sub.py
@@ -1,41 +1,24 @@
 def calseq(infile,outfile1,outfile2):
-    fasta={}
-    fa_term=[]
-    with open (infile) as f1:
-        for line in f1 :
-            line = line.strip()
-            if line[0]=='>':
-                if fa_term:
-                    seq=''.join(fa_term)
-                    if seq in fasta.keys():
-                        fasta[seq][1]+=1
-                        fa_term=[]
-                    else:
-                        fasta[seq]=[]
-                        fasta[seq].append(ID)
-                        fasta[seq].append(1)
-                        fa_term=[]
-                ID=line
-            else:
-                fa_term.append(line)
-        seq=''.join(fa_term)
-        if seq in fasta.keys():
-            fasta[seq][1]+=1
+    from fasta import fasta_iter
+    import gzip
+    fasta = {}
+    for ID,seq in fasta_iter(infile):
+        if seq in fasta:
+            fasta[seq][1] += 1
         else:
-            fasta[seq]=[]
-            fasta[seq].append(ID)
-            fasta[seq].append(1)
-    out1=open(outfile1, "w")
-    out2=open(outfile2, "w")
-    for key,value in fasta.items():
-        out1.write(str(value[1])+"\t"+key+"\n")
-        out2.write(value[0]+"\n"+key+"\n")
+            fasta[seq] = [ID, 1]
+    out1 = gzip.open(outfile1, "wt", compresslevel=1)
+    out2 =  gzip.open(outfile2, "wt", compresslevel=1)
+    for seq,(ID,count) in fasta.items():
+        out1.write(f"{count}\t{seq}\n")
+        out2.write(f">{ID}\n{seq}\n")
     out1.close()
-    out2.close()    
-if __name__ == '__main__':            
+    out2.close()
+
+if __name__ == '__main__':
     import sys
     import os
-    infile="/home1/duanyq/GMSC_10/submetag5.faa"
-    outfile1="/home1/duanyq/GMSC_10/submetag5_raw_number.txt"
-    outfile2="/home1/duanyq/GMSC_10/submetag5_dedup.faa"
+    infile="/home1/duanyq/GMSC_10/submetag5.faa.gz"
+    outfile1="/home1/duanyq/GMSC_10/submetag5_raw_number.txt.gz"
+    outfile2="/home1/duanyq/GMSC_10/submetag5_dedup.faa.gz"
     calseq(infile,outfile1,outfile2)
