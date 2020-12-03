8dd7c167e15e08b702b48492cbbd556f12653aee
cocodyq <55238881+cocodyq@users.noreply.github.com>
2020-12-03 13:10:40 +0800

    merge sorted smORF

---
+++ GMSC_10/4_merge.py
@@ -0,0 +1,20 @@
+from jug import TaskGenerator, bvalue
+
+@TaskGenerator
+def mergeseq(outfile):
+    import heapq
+    import gzip
+    from glob import glob
+    from fasta import fasta_iter
+
+    with gzip.open(outfile, compresslevel=1, mode='wt') as output:
+        inputs = [fasta_iter(f) for f in glob(f'submetag*.dedup.faa.gz')]
+        merged = heapq.merge(*inputs, key=lambda h_seq: (h_seq[1], h_seq[0]))
+        preseq="x"
+        for h,seq in merged:
+            if seq != preseq:
+                output.write(f'>{h}\n{seq}\n')
+                preseq = seq
+
+OUTPUT_FILE="smorf_dedup.faa.gz"
+mergeseq(OUTPUT_FILE)
