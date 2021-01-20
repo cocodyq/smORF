1d1b5c2e0f800d5511b76ce9e28b105d449c3e22
cocodyq <55238881+cocodyq@users.noreply.github.com>
2021-01-20 17:10:26 +0800

    Hash split & dedup & sort

--- GMSC_10/1_deduplicate.py
+++ GMSC_10/1_deduplicate.py
@@ -1,31 +1,34 @@
 from jug import TaskGenerator, bvalue
+import hashlib
+import gzip 
+from fasta import fasta_iter
 
 @TaskGenerator
-def splitseq(infile,X,outfile):
-    '''split inputfile according to max number of sequences X'''
-    import gzip
-    f = gzip.open(infile,"rt")
-    ix = 0
-    n = X + 1
-    onames = []
-    while True:
-        index = f.readline().strip()
-        seq = f.readline().strip()
-        if not seq:
-            break
-        if n > X:
-            oname = outfile.format(ix=ix)
-            onames.append(oname)
-            out =  gzip.open(oname, "wt", compresslevel=1)
-        out.write(index+"\n"+seq+"\n")
-    out.close()
-    return onames
+def splitseq(infile):
+    print("start splitseq")
+    outputlist = [
+        f'split/submetag_{ix:03}.faa.gz'
+        for ix in range(256)]
+    outputfiles = [
+        gzip.open(f'split/submetag_{ix:03}.faa.gz',compresslevel=1,  mode='wt')
+        for ix in range(256)]
+    for ID,seq in fasta_iter(infile):
+        h=ID
+        h = hashlib.sha256()
+        h.update(seq.encode('ascii'))
+        ix = int(h.hexdigest()[:2], 16)
+        outputfiles[ix].write(f'>{ID}\n{seq}\n')
+    for ot in outputfiles:
+        ot.close()
+    print("finish splitseq")
+    return (outputlist)
     
 @TaskGenerator
 def dedup_fasta(infile):
     from fasta import fasta_iter
     import gzip
     import os
+    print("start dedup")
     fasta = {}
     for ID,seq in fasta_iter(infile):
         if seq in fasta:
@@ -37,18 +40,19 @@ def dedup_fasta(infile):
     outfile2 = sp.replace('.faa.gz', '.dedup.faa.gz')
     out1 = gzip.open(outfile1, "wt", compresslevel=1)
     out2 = gzip.open(outfile2, "wt", compresslevel=1)
+    print("start sort")
     for seq,(ID,count) in sorted(fasta.items()):
         out1.write(f"{count}\t{seq}\n")
         out2.write(f">{ID}\n{seq}\n")
     out1.close()
     out2.close()
     os.unlink(infile)
+    print("finish dedup and sort")
     return (outfile1, outfile2)
 
-INPUT_FILE = "data/GMSC10.metag_smorf.faa.gz"
-SPLIT_FILE_PAT = "splits/submetag{ix}.faa.gz"
+INPUT_FILE = "data/GMSC10.metag_smorfs.faa.gz"
 
-splits = splitseq(INPUT_FILE, 1_000_000_000, SPLIT_FILE_PAT)
+splits = splitseq(INPUT_FILE)
 
 for sp in bvalue(splits):
     dedup_fasta(sp)
