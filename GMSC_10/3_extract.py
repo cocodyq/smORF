adcc85f04dcab0f6bd2715cfc02e3df0c35ded07
Luis Pedro Coelho <luis@luispedro.org>
2021-01-21 15:26:39 +0800

    RFCT Better code
    - use with where possible
    - use set() where appropriate

--- GMSC_10/3_extract.py
+++ GMSC_10/3_extract.py
@@ -5,24 +5,21 @@ def extract_seq(infile1,infile2,outfile1,outfile2):
     from fasta import fasta_iter
     import gzip
     fasta={}
-    f = gzip.open(infile1,"rt")
-    for line in f:
-        line = line.strip()
-        linelist = line.split("\t")
-        if linelist[0] != "1":
-            fasta[linelist[1]] = ""
-    f.close()
-    
-    out1 = gzip.open(outfile1, "wt", compresslevel=1)
-    out2 = gzip.open(outfile2, "wt", compresslevel=1)
-    for ID,seq in fasta_iter(infile2):
-        if seq in fasta.keys():
-            out1.write(f'>{ID}\n{seq}\n')
-        else:
-            out2.write(f'>{ID}\n{seq}\n')
-    out1.close()
-    out2.close()
-    
+    with gzip.open(infile1,"rt") as f:
+        for line in f:
+            line = line.strip()
+            linelist = line.split("\t")
+            if linelist[0] != "1":
+                fasta[linelist[1]] = ""
+
+    with gzip.open(outfile1, "wt", compresslevel=1) as out1, \
+        gzip.open(outfile2, "wt", compresslevel=1) as out2:
+        for ID,seq in fasta_iter(infile2):
+            if seq in fasta.keys():
+                out1.write(f'>{ID}\n{seq}\n')
+            else:
+                out2.write(f'>{ID}\n{seq}\n')
+
 
 infile1="data/pro_raw_number.tsv.gz"
 infile2="data/pro_dedup.faa.gz"
--- GMSC_10/9.1_extract_0.9.py
+++ GMSC_10/9.1_extract_0.9.py
@@ -1,21 +1,19 @@
 def extract(infile1,infile2,outfile1,outfile2):
-    fasta={}
+    fasta=set()
     with open (infile1) as f1:
         for line in f1 :
             line = line.strip()
-            fasta[line]=""
-            
-    out1=open(outfile1,"w")
-    out2=open(outfile2,"w")
-    for seq_record in SeqIO.parse(infile2, "fasta"):
-        if str(seq_record.id) in fasta.keys():
-            out1.write(f'>{seq_record.id}\n{seq_record.seq}\n')
-        else:
-            out2.write(f'>{seq_record.id}\n{seq_record.seq}\n')   
-    out1.close()
-    out2.close()
-    
-if __name__ == '__main__': 
+            fasta.add(line)
+
+    with open(outfile1,"w") as out1, \
+        open(outfile2,"w") as out2:
+        for seq_record in SeqIO.parse(infile2, "fasta"):
+            if str(seq_record.id) in fasta:
+                out1.write(f'>{seq_record.id}\n{seq_record.seq}\n')
+            else:
+                out2.write(f'>{seq_record.id}\n{seq_record.seq}\n')
+
+if __name__ == '__main__':
     import Bio
     from Bio import SeqIO
 
