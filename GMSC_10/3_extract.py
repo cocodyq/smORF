fced72fc3d1192ebf0834cd31ebe821c1d961711
cocodyq <55238881+cocodyq@users.noreply.github.com>
2021-01-23 16:40:09 +0800

    extract singletons and nonsingletons
    change dict to set

--- GMSC_10/3_extract.py
+++ GMSC_10/3_extract.py
@@ -4,18 +4,18 @@ from jug import TaskGenerator
 def extract_seq(infile1,infile2,outfile1,outfile2):
     from fasta import fasta_iter
     import gzip
-    fasta={}
+    fastaset=set()
     with gzip.open(infile1,"rt") as f:
         for line in f:
             line = line.strip()
             linelist = line.split("\t")
             if linelist[0] != "1":
-                fasta[linelist[1]] = ""
+                fastaset.add(linelist[1])
 
     with gzip.open(outfile1, "wt", compresslevel=1) as out1, \
         gzip.open(outfile2, "wt", compresslevel=1) as out2:
         for ID,seq in fasta_iter(infile2):
-            if seq in fasta.keys():
+            if seq in fastaset:
                 out1.write(f'>{ID}\n{seq}\n')
             else:
                 out2.write(f'>{ID}\n{seq}\n')
@@ -23,6 +23,6 @@ def extract_seq(infile1,infile2,outfile1,outfile2):
 
 infile1="data/pro_raw_number.tsv.gz"
 infile2="data/pro_dedup.faa.gz"
-outfile1="data/pro_nonsingleton.faa.gz"
-outfile2="data/pro_singleton.faa.gz"
+outfile1="data/nonsingleton.faa.gz"
+outfile2="data/singleton.faa.gz"
 extract_seq(infile1,infile2,outfile1,outfile2)
