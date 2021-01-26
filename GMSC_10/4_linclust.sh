118c51e2751e25427eba6b85d143ba3680cbc89f
cocodyq <55238881+cocodyq@users.noreply.github.com>
2021-01-26 16:12:24 +0800

    linclust

---
+++ GMSC_10/4_linclust.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/env bash
+
+set -e
+set -o pipefail
+
+mkdir clust_result
+cd clust_result
+
+#make db
+mmseqs createdb ../data/metag_ProG_nonsingleton.faa.gz metag_ProG_nonsingleton.DB
+
+mkdir 0.9_result
+cd 0.9_result
+
+#clust with kmer:21,-c 0.9,--min-seq-id:0.9
+mmseqs linclust ../metag_ProG_nonsingleton.DB metag_ProG_nonsingleton_0.9_clu tmp -c 0.9 --min-seq-id 0.9 
+
+#Extract representative sequence
+mmseqs createsubdb metag_ProG_nonsingleton_0.9_clu ../metag_ProG_nonsingleton.DB metag_ProG_nonsingleton_0.9_clu_rep 
+
+mmseqs convert2fasta metag_ProG_nonsingleton_0.9_clu_rep  metag_ProG_nonsingleton_0.9_clu_rep.faa
+
+#generate tsv
+mmseqs createtsv ../metag_ProG_nonsingleton.DB ../metag_ProG_nonsingleton.DB metag_ProG_nonsingleton_0.9_clu metag_ProG_nonsingleton_0.9_clu.tsv
+
+cd ..
+mkdir 0.5_result
+cd 0.5_result
+
+#clust with kmer:21,-c 0.9,--min-seq-id:0.5
+mmseqs linclust ../metag_ProG_nonsingleton.DB metag_ProG_nonsingleton_0.5_clu tmp -c 0.9 --min-seq-id 0.5
+
+#Extract representative sequence
+mmseqs createsubdb metag_ProG_nonsingleton_0.5_clu ../metag_ProG_nonsingleton.DB metag_ProG_nonsingleton_0.5_clu_rep 
+
+mmseqs convert2fasta metag_ProG_nonsingleton_0.5_clu_rep  metag_ProG_nonsingleton_0.5_clu_rep.faa
+
+#generate tsv
+mmseqs createtsv ../metag_ProG_nonsingleton.DB ../metag_ProG_nonsingleton.DB metag_ProG_nonsingleton_0.5_clu metag_ProG_nonsingleton_0.5_clu.tsv
