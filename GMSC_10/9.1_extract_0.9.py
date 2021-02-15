efa971a80a284e2cf139fe2afb6f165b9f0d2cd0
cocodyq <55238881+cocodyq@users.noreply.github.com>
2021-02-15 23:35:02 +0800

    5_extract singleton/nonsingleton

--- GMSC_10/9.1_extract_0.9.py
+++ GMSC_10/9.1_extract_0.9.py
@@ -1,24 +1,28 @@
 def extract(infile1,infile2,outfile1,outfile2):
-    fasta=set()
+    fastaset=set()
     with open (infile1) as f1:
         for line in f1 :
             line = line.strip()
-            fasta.add(line)
-
+            fastaset.add(line)
+    
     with open(outfile1,"w") as out1, \
         open(outfile2,"w") as out2:
-        for seq_record in SeqIO.parse(infile2, "fasta"):
-            if str(seq_record.id) in fasta:
-                out1.write(f'>{seq_record.id}\n{seq_record.seq}\n')
+        for ID,seq in fasta_iter(infile2):
+            if ID in fastaset:
+                out1.write(f'>{ID}\n{seq}\n')
             else:
-                out2.write(f'>{seq_record.id}\n{seq_record.seq}\n')
+                out2.write(f'>{ID}\n{seq}\n')
 
 if __name__ == '__main__':
-    import Bio
-    from Bio import SeqIO
+    from fasta import fasta_iter
 
-    infile1="/home1/duanyq/GMSC_10/linclust/0.9/0.9clu_singleton_name"
-    infile2="/home1/duanyq/GMSC_10/linclust/0.9/GMSC10_smorf_nonsingleton_0.9_clu_rep.faa"
-    outfile1="/home1/duanyq/GMSC_10/linclust/0.9/0.9clu_singleton.faa"
-    outfile2="/home1/duanyq/GMSC_10/linclust/0.9/0.9clu_nonsingleton.faa"
+    infile1="clust_result/0.9_result/0.9clu_singleton_name"
+    infile2="clust_result/0.9_result/metag_ProG_nonsingleton_0.9_clu_rep.faa"
+    infile3="clust_result/0.5_result/0.5clu_singleton_name"
+    infile4="clust_result/0.5_result/metag_ProG_nonsingleton_0.5_clu_rep.faa"
+    outfile1="clust_result/0.9_result/0.9clu_singleton.faa"
+    outfile2="clust_result/0.9_result/0.9clu_nonsingleton.faa"
+    outfile3="clust_result/0.5_result/0.5clu_singleton.faa"
+    outfile4="clust_result/0.5_result/0.5clu_nonsingleton.faa"
     extract(infile1,infile2,outfile1,outfile2)
+    extract(infile3,infile4,outfile3,outfile4)
