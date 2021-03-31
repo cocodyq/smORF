69d7f2f89f15ccabb3e8880537acfd8294a717e7
cocodyq <55238881+cocodyq@users.noreply.github.com>
2021-04-01 00:24:25 +0800

    Analysis of diamond result (add coverage)

--- GMSC_10/11_add_coverage.sh
+++ GMSC_10/11_add_coverage.sh
@@ -5,7 +5,7 @@ set -o pipefail
 
 cd diamond/analysis/analysis_0.5
 
-for i in {1..23}
+for i in {0..23}
   do
   cat sub${i}.faa.gz.tsv.tmp.1 | awk '{print $0"\t"$6/$2"\t"$6/$4}' > sub${i}.faa.gz.tsv.tmp.2
   awk '$15 >= 0.9 && $16 >= 0.9' sub${i}.faa.gz.tsv.tmp.2 > sub${i}.faa.gz.tsv.tmp.3
@@ -14,7 +14,7 @@ for i in {1..23}
 
 cd diamond/analysis/analysis_0.9
 
-for i in {1..23}
+for i in {0..23}
   do
   cat sub${i}.faa.gz.tsv.tmp.1 | awk '{print $0"\t"$6/$2"\t"$6/$4}' > sub${i}.faa.gz.tsv.tmp.2
   awk '$15 >= 0.9 && $16 >= 0.9' sub${i}.faa.gz.tsv.tmp.2 > sub${i}.faa.gz.tsv.tmp.3
