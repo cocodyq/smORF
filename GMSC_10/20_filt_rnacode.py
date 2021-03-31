6facffd2cf9cc49a8380b22e7b23db0f2671f659
cocodyq <55238881+cocodyq@users.noreply.github.com>
2021-04-01 00:55:32 +0800

    Analysis of RNAcode (filt results < 0.05)

---
+++ GMSC_10/20_filt_rnacode.py
@@ -0,0 +1,24 @@
+def file_name(file_dir,outfile):
+    import os
+    out = open(outfile, "w")
+    for n in range(1,288):
+        print("first"+str(n)+"\n")
+        first_dir = file_dir+"/first"+str(n)
+        for m in range(1,301):
+            print("second"+str(m)+"\n")
+            second_dir = first_dir+"/second"+str(m)
+            for infile in os.listdir(second_dir):
+                file_path = second_dir+"/"+infile
+                with open (file_path) as f1:
+                    for line in f1 :
+                        line = line.strip()
+                        linelist = line.split("\t")
+                        if float(linelist[-1]) < 0.05:
+                            filesplit = infile.split(".")
+                            name = filesplit[0]+"."+filesplit[1]+"."+filesplit[2]
+                            out.write(name+"\n")
+                            break
+    out.close()
+file_dir = "RNAcode_result/rna"
+outfile = "RNAcode_result/filt/result/smORF_0.9_RNAcode.tsv"
+file_name(file_dir,outfile)
